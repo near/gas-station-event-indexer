fd66b29612d148d59fa65bc88e65f64b033e85de
Ben Smith <bh2smith@gmail.com>
2024-04-21 22:32:38 +0200

    add pylintrc file and modify indexer to remove errors

--- gas_station_event_indexer.py
+++ gas_station_event_indexer.py
@@ -1,41 +1,52 @@
 import asyncio
 import json
 import os
+from typing import Optional
 import requests
 import toml
 
 from near_lake_framework import near_primitives, LakeConfig, streamer
 
+REQUEST_TIMEOUT = 10
+
 # load the config
-with open('config.toml', 'r') as file:
+with open("config.toml", "r", encoding="utf-8") as file:
     config = toml.load(file)
     expected_config_keys = ["network", "contract_id"]
-    has_all_necessary_config_keys = all(exp_key in config for exp_key in expected_config_keys)
+    has_all_necessary_config_keys = all(
+        exp_key in config for exp_key in expected_config_keys
+    )
     if not has_all_necessary_config_keys:
         raise ValueError(f"Missing keys in config.toml: {expected_config_keys}")
 
 
-def fetch_latest_block(network: str = 'mainnet'):
-    # Define the RPC endpoint for the NEAR network
-    url = "https://rpc.mainnet.near.org" if network == 'mainnet' else "https://rpc.testnet.near.org"
+def fetch_latest_block(network: str = "mainnet"):
+    """
+    Define the RPC endpoint for the NEAR network
+    """
+    url = (
+        "https://rpc.mainnet.near.org"
+        if network == "mainnet"
+        else "https://rpc.testnet.near.org"
+    )
 
     # Define the payload for fetching the latest block
-    payload = json.dumps({
-        "jsonrpc": "2.0",
-        "id": "dontcare",
-        "method": "block",
-        "params": {
-            "finality": "final"
+    payload = json.dumps(
+        {
+            "jsonrpc": "2.0",
+            "id": "dontcare",
+            "method": "block",
+            "params": {"finality": "final"},
         }
-    })
+    )
 
     # Define the headers for the HTTP request
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    headers = {"Content-Type": "application/json"}
 
     # Send the HTTP request to the NEAR RPC endpoint
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request(
+        "POST", url, headers=headers, data=payload, timeout=REQUEST_TIMEOUT
+    )
 
     # Parse the JSON response to get the latest final block height
     latest_final_block = response.json()["result"]["header"]["height"]
@@ -58,84 +69,169 @@ def fetch_latest_block(network: str = 'mainnet'):
 #     }
 # }
 
-def valdiate_event_data(event_data: dict) -> bool:
+
+def validate_event_data(event_data: dict) -> bool:
     expected_event_keys = ["foreign_chain_id", "signed_transactions"]
-    has_all_necessary_event_keys = all(exp_key in event_data for exp_key in expected_event_keys)
-    return has_all_necessary_event_keys and len(event_data.get("signed_transactions", [])) == 2
+    has_all_necessary_event_keys = all(
+        exp_key in event_data for exp_key in expected_event_keys
+    )
+    return (
+        has_all_necessary_event_keys
+        and len(event_data.get("signed_transactions", [])) == 2
+    )
+
+
+def extract_relevant_log(
+    log: str, receipt_id: near_primitives.CryptoHash
+) -> Optional[dict]:
+    if not log.startswith("EVENT_JSON:"):
+        return None
+
+    try:
+        parsed_log = json.loads(log[len("EVENT_JSON:") :])
+    except json.JSONDecodeError:
+        print(
+            f"Receipt ID: `{receipt_id}`\n"
+            f"Error during parsing logs from JSON string to dict"
+        )
+        return None
+
+    if (
+        parsed_log.get("standard") != "x-gas-station"
+        or parsed_log.get("event") != "transaction_sequence_signed"
+    ):
+        return None
+    return parsed_log
+
+
+def process_shard(shard: near_primitives.IndexerShard):
+    for receipt_execution_outcome in shard.receipt_execution_outcomes:
+        process_receipt_execution_outcome(receipt_execution_outcome)
+
+
+def process_receipt_execution_outcome(
+    receipt_execution_outcome: near_primitives.IndexerExecutionOutcomeWithReceipt,
+):
+    for log in receipt_execution_outcome.execution_outcome.outcome.logs:
+        receipt = receipt_execution_outcome.receipt
+        if not process_log(log, receipt):
+            continue
+
+
+def process_log(log: str, receipt: near_primitives.Receipt):
+    parsed_log = extract_relevant_log(log, receipt.receipt_id)
+    if parsed_log is None:
+        return False
+
+    print(json.dumps(parsed_log, indent=4))
+    return process_receipt_if_gas_station_contract(receipt, parsed_log)
+
+
+def process_receipt_if_gas_station_contract(
+    receipt: near_primitives.Receipt, parsed_log: dict
+) -> bool:
+    if not receipt.receiver_id.endswith(config.get("contract_id")):
+        return False
+
+    try:
+        parsed_event_data = parsed_log["data"]
+        if not validate_event_data(parsed_event_data):
+            print(f"Error: Invalid event data: {parsed_event_data}")
+            return False
+
+        print(json.dumps(parsed_event_data, indent=4))
+        send_event_data_to_service(parsed_event_data)
+        return True
+
+    except json.JSONDecodeError:
+        print(
+            f"Receipt ID: `{receipt.receipt_id}`\nError during parsing event data from JSON string to dict"
+        )
+        return False
+
+
+def send_event_data_to_service(event_data: dict):
+    payload = {
+        "foreign_chain_id": event_data["foreign_chain_id"],
+        "signed_transactions": event_data["signed_transactions"],
+    }
+    url = "localhost:3030/send_funding_and_user_signed_txns"
+    try:
+        response = requests.post(url, json=payload, timeout=REQUEST_TIMEOUT)
+        if response.status_code not in {200, 201}:
+            print(f"Error: calling {url}: {response.text}")
+        else:
+            print(f"Response from {url}: {response.text}")
+    except requests.RequestException as e:
+        print(f"HTTP Request failed: {str(e)}")
 
 
 async def handle_streamer_message(streamer_message: near_primitives.StreamerMessage):
     for shard in streamer_message.shards:
-        for receipt_execution_outcome in shard.receipt_execution_outcomes:
-            for log in receipt_execution_outcome.execution_outcome.outcome.logs:
-                if not log.startswith("EVENT_JSON:"):
-                    continue
-                try:
-                    parsed_log = json.loads(log[len("EVENT_JSON:"):])
-                except json.JSONDecodeError:
-                    print(
-                        f"Receipt ID: `{receipt_execution_outcome.receipt.receipt_id}`\n"
-                        f"Error during parsing logs from JSON string to dict"
-                    )
-                    continue
+        process_shard(shard)
+    # for shard in streamer_message.shards:
+    #     for receipt_execution_outcome in shard.receipt_execution_outcomes:
+    #         for log in receipt_execution_outcome.execution_outcome.outcome.logs:
+    #             receipt = receipt_execution_outcome.receipt
 
-                if (
-                        parsed_log.get("standard") != "x-gas-station"
-                        or parsed_log.get("event") != "transaction_sequence_signed"
-                ):
-                    continue
-                else:
-                    print(json.dumps(parsed_log, indent=4))
+    #             parsed_log = extract_relevant_log(log, receipt.receipt_id)
+    #             if parsed_log is None:
+    #                 continue
 
-                if receipt_execution_outcome.receipt.receiver_id.endswith(
-                        config.get("contract_id")
-                ):  # gas station contract account id
-                    try:
-                        parsed_event_data: dict = parsed_log["data"]
-                        if not valdiate_event_data(parsed_event_data):
-                            print(f"Error: Invalid event data: {parsed_event_data}")
-                        else:
-                            payload = {
-                                "foreign_chain_id": parsed_event_data["foreign_chain_id"],
-                                "signed_transactions": parsed_event_data["signed_transactions"],
-                            }
-                            response = requests.post(
-                                url="localhost:3030/send_funding_and_user_signed_txns",
-                                json=payload,
-                            )
-                            if response.status_code not in {200, 201}:
-                                print(f"Error: calling localhost:3030/send_funding_and_user_signed_txns: {response.text}")
-                            else:
-                                print(f"Response from localhost:3030/send_funding_and_user_signed_txns: {response.text}")
-                    except json.JSONDecodeError:
-                        print(
-                            f"Receipt ID: `{receipt_execution_outcome.receipt.receipt_id}`\n"
-                            f"Error during parsing event data from JSON string to dict"
-                        )
-                        continue
+    #             print(json.dumps(parsed_log, indent=4))
 
-                else:
-                    continue
+    #             # gas station contract account id
+    #             if receipt.receiver_id.endswith(config.get("contract_id")):
+    #                 try:
+    #                     parsed_event_data: dict = parsed_log["data"]
+    #                     if not valdiate_event_data(parsed_event_data):
+    #                         print(f"Error: Invalid event data: {parsed_event_data}")
+    #                     else:
+    #                         payload = {
+    #                             "foreign_chain_id": parsed_event_data["foreign_chain_id"],
+    #                             "signed_transactions": parsed_event_data["signed_transactions"],
+    #                         }
+    #                         url = "localhost:3030/send_funding_and_user_signed_txns"
+    #                         response = requests.post(
+    #                             url,
+    #                             json=payload,
+    #                             timeout=REQUEST_TIMEOUT,
+    #                         )
+    #                         if response.status_code not in {200, 201}:
+    #                             print(f"Error: calling {url}: {response.text}")
+    #                         else:
+    #                             print(f"Response from {url}: {response.text}")
+    #                 except json.JSONDecodeError:
+    #                     print(
+    #                         f"Receipt ID: `{receipt.receipt_id}`\n"
+    #                         f"Error during parsing event data from JSON string to dict"
+    #                     )
+    #                     continue
 
-                print(json.dumps(parsed_event_data, indent=4))
+    #             else:
+    #                 continue
+
+    #             print(json.dumps(parsed_event_data, indent=4))
 
 
-async def main():
+async def main() -> None:
     if config.get("network") == "mainnet":
         lake_config = LakeConfig.mainnet()
-        latest_final_block = fetch_latest_block(network='mainnet')
+        latest_final_block = fetch_latest_block(network="mainnet")
     elif config.get("network") == "testnet":
         lake_config = LakeConfig.testnet()
-        latest_final_block = fetch_latest_block(network='testnet')
+        latest_final_block = fetch_latest_block(network="testnet")
     else:
         raise ValueError(f"Unknown network: {config.get('network')}")
 
-    print(f"Latest final block: {latest_final_block} on network: {config.get('network')}")
+    print(
+        f"Latest final block: {latest_final_block} on network: {config.get('network')}"
+    )
     lake_config.start_block_height = latest_final_block
     lake_config.aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
     lake_config.aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 
-    stream_handle, streamer_messages_queue = streamer(lake_config)
+    _stream_handle, streamer_messages_queue = streamer(lake_config)
     while True:
         streamer_message = await streamer_messages_queue.get()
         await handle_streamer_message(streamer_message)
